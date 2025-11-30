18830db2b1ddf02f8ed9e4c1389be673da0af6a3
Tanvi Agrawal <tanviagrawal9913@gmail.com>
2025-11-30 22:14:40 +0530

    add new page

--- CounterApp/src/Components/Reset/ResetCount.jsx
+++ CounterApp/src/Components/Reset/ResetCount.jsx
@@ -4,15 +4,10 @@ import Navbar from '../Navbar/Navbar';
 const Reset = () => {
   const [count,setCount]=useState(0);
 
-  const handleCount=()=>{
-    setCount(count);
-  }
   return (
     <div>
-      <Navbar/>
-      <h1>Reset Count Component</h1>
-      <h2>{count}</h2>
-      <button onClick={handleCount}>Reset</button>
+      {/* <Navbar/> */}
+      <button onClick={()=>setCount(count)}>Reset</button>
     </div>
   )
 }
--- CounterApp/src/Pages/UniformCountOperation/UniformCountOperation.jsx
+++ CounterApp/src/Pages/UniformCountOperation/UniformCountOperation.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import Reset from '../../Components/Reset/ResetCount';
+import './UniformCountOperation.css'
 
 const UniformCountOperation = () => {
+  const[count,setCount]=useState(0);
     
   return (
-    <div>
+    <div className="uni-container">
       <Navbar/>
-      <h1>Uniform Count Page</h1>
+
+      <div className="uni-wrap" >
+        <h1 className="unicount-heading" >Uniform Count Page</h1>
+        <h2 className="count1" > Count: {count} </h2>
+
+        <div className="buttons">
+        <button className="count-btns" onClick={()=>setCount(count+1)} >Increase</button>
+        <button className="count-btns" onClick={()=>setCount(count-1)}  >Decrease</button>
+        <button className="count-btns" onClick={()=>setCount(0)}>Reset</button>
+        </div>        
+    
+      </div>
     </div>
   )
 }
