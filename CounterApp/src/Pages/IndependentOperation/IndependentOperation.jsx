b59e79cc8860e968c2151ce51a18826cb7583e1f
Tanvi Agrawal <tanviagrawal9913@gmail.com>
2025-12-01 22:58:59 +0530

    add independent count page

--- CounterApp/src/Pages/IndependentOperation/IndependentOperation.jsx
+++ CounterApp/src/Pages/IndependentOperation/IndependentOperation.jsx
@@ -13,33 +13,35 @@ const IndependentOperation = () => {
   }
 
   return (
-    <div className="ind-container">
+   <div className="ind-container">
       <Navbar/>
+      
       <div className="ind-wrap">
         <h1 className="ind-heading">Independent Count Page</h1>
 
-        <div className="ind-section">
-  <h2 className="indcount">Increase count: {count}</h2>
-    <h2 className="indcount">Decrease count: {count1}</h2>
-    </div>
-            
-           <div className="inc-btn">
-           
-             <button className="ind-btn" onClick={()=>setCount(count+1)} >Increase</button>
-             <button className="ind-btn" onClick={()=>handleReset("inc")} >Rest</button>
-           </div>
-           
-           
-           <div className="dec-btn">
-          
-             <button className="ind-btn" onClick={()=>setCount1(count1-1)} >Decrease</button>
-             <button className="ind-btn" onClick={()=>handleReset("dec")} >Reset</button>
-           </div>
+        <div className="ind-row-top">
+
+          <div className="ind-section">
+            <h2 className="indcount">Increase count: {count}</h2>
+            <div className="inc-btn">
+                <button className="ind-btn" onClick={()=>setCount(count+1)}>Increase</button>
+                <button className="ind-btn" onClick={()=>handleReset("inc")}>Reset</button>
+            </div>
+          </div>
+
+          <div className="ind-section1">
+            <h2 className="indcount">Decrease count: {count1}</h2>
+            <div className="dec-btn">
+                <button className="ind-btn" onClick={()=>setCount1(count1-1)}>Decrease</button>
+                <button className="ind-btn" onClick={()=>handleReset("dec")}>Reset</button>
+            </div>
+          </div>
 
         </div>
 
       </div>
-  
+</div>
+
   )
 }
 
